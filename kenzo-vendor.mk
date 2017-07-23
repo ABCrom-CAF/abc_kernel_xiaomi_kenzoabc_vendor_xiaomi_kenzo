e4faf4ca170f9a364d8f223f7ef00b74e8fbc2df
Bill Chen <billchen1977@gmail.com>
2017-07-23 14:34:36 +0000

    Import performance boost support lib
    Change-Id: I82f22789022ebdd6a0996b017500be169e8de40b
    Signed-off-by: thedarkstrix <thedarkstrix@gmail.com>

--- kenzo-vendor.mk
+++ kenzo-vendor.mk
@@ -198,6 +198,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/kenzo/proprietary/vendor/bin/slim_daemon:system/vendor/bin/slim_daemon \
     vendor/xiaomi/kenzo/proprietary/vendor/bin/thermal-engine:system/vendor/bin/thermal-engine \
     vendor/xiaomi/kenzo/proprietary/vendor/etc/diracmobile.config:system/vendor/etc/diracmobile.config \
+    vendor/xiaomi/kenzo/proprietary/vendor/etc/public.libraries.txt:system/vendor/etc/public.libraries.txt \
     vendor/xiaomi/kenzo/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:system/vendor/lib/egl/eglSubDriverAndroid.so \
     vendor/xiaomi/kenzo/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
@@ -915,3 +916,6 @@ PRODUCT_PACKAGES += \
     imssettings \
     qcnvitems \
     qcrilhook
+    com.qualcomm.qti.Performance.xml \
+    libqti_performance \
+    QPerformance
